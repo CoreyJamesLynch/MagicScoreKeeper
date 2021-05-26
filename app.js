6435458a383a965da6b3ca6284ec6dd13367a19e
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-26 15:58:29 -0400

    Add plus/minus life button logic

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-alert('I am connected');
+const player1AddLife = document.querySelector('#add1Player1');
+const player1MinusLife = document.querySelector('#minus1Player1');
+const player2AddLife = document.querySelector('#add1Player2');
+const player2MinusLife = document.querySelector('#minus1Player2');
+const player1Life = document.querySelector('#player1Score');
+const player2Life = document.querySelector('#player2Score');
+
+let player1LifeTotal = 20;
+let player2LifeTotal = 20;
+
+player1AddLife.addEventListener('click', function (event) {
+  event.preventDefault();
+  player1LifeTotal += 1;
+  player1Life.innerText = parseInt(player1LifeTotal);
+});
+
+player1MinusLife.addEventListener('click', function (event) {
+  event.preventDefault();
+  player1LifeTotal -= 1;
+  player1Life.innerText = parseInt(player1LifeTotal);
+});
+
+player2AddLife.addEventListener('click', function (event) {
+  event.preventDefault();
+  player2LifeTotal += 1;
+  player2Life.innerText = parseInt(player2LifeTotal);
+});
+
+player2MinusLife.addEventListener('click', function (event) {
+  event.preventDefault();
+  player2LifeTotal -= 1;
+  player2Life.innerText = parseInt(player2LifeTotal);
+});
