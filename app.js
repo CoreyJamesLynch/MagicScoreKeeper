9145bb3cfd0ab85ba2c0eef2ca3e12ba62ae0a4f
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-26 16:02:25 -0400

    Add reset logic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const player2AddLife = document.querySelector('#add1Player2');
 const player2MinusLife = document.querySelector('#minus1Player2');
 const player1Life = document.querySelector('#player1Score');
 const player2Life = document.querySelector('#player2Score');
+const resetButton = document.querySelector('#gameReset');
 
 let player1LifeTotal = 20;
 let player2LifeTotal = 20;
@@ -31,3 +32,11 @@ player2MinusLife.addEventListener('click', function (event) {
   player2LifeTotal -= 1;
   player2Life.innerText = parseInt(player2LifeTotal);
 });
+
+resetButton.addEventListener( 'click' ,function (event){
+  event.preventDefault();
+  player1LifeTotal = 20;
+  player2LifeTotal = 20;
+  player1Life.innerText = parseInt(player1LifeTotal);
+  player2Life.innerText = parseInt(player2LifeTotal);
+})
